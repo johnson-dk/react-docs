354df8f8df42d7b10683803d29b6c622b84a1b22
Brian Duncan Johnson <brian.duncan.johnson@sallinggroup.com>
2019-07-14 17:17:01 +0200

    Introducing JSX

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(
-    <h1>Hello, world!</h1>,
-    document.getElementById('root')
-  );
+function formatName(user) {
+  return user.firstName + " " + user.lastName;
+}
+
+const user = {
+  firstName: "Joe",
+  lastName: "Bloggs"
+};
+
+const element = <h1>Hello, {formatName(user)}!</h1>;
+
+ReactDOM.render(element, document.getElementById("root"));
