1df1fbcf8790312f6ce10dc28a63c7a07512e5a9
Brian Duncan Johnson <brian.duncan.johnson@sallinggroup.com>
2019-07-16 14:01:52 +0200

    Rendering elements

--- src/index.js
+++ src/index.js
@@ -9,7 +9,14 @@ const user = {
   firstName: "Joe",
   lastName: "Bloggs"
 };
+function tick() {
+  const element = (
+    <div>
+      <h1>Hello, {formatName(user)}!</h1>
+      <h2>It is {new Date().toLocaleTimeString()}.</h2>
+    </div>
+  );
+  ReactDOM.render(element, document.getElementById("root"));
+}
 
-const element = <h1>Hello, {formatName(user)}!</h1>;
-
-ReactDOM.render(element, document.getElementById("root"));
+setInterval(tick, 1000);
