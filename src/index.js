4b1d3648a7d2a867177aca2902b3a7cbfc1293db
Brian Duncan Johnson <brian.duncan.johnson@sallinggroup.com>
2019-07-17 20:25:38 +0200

    Handling events

--- src/index.js
+++ src/index.js
@@ -40,7 +40,34 @@ class Clock extends React.Component {
   }
 
   render() {
-    return <h2>It is <FormattedDate date={this.state.date}/>.</h2>;
+    return (
+      <h2>
+        It is <FormattedDate date={this.state.date} />.
+      </h2>
+    );
+  }
+}
+
+class Toggle extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { isToggleOn: true };
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState(state => ({
+      isToggleOn: !state.isToggleOn
+    }));
+  }
+
+  render() {
+    return (
+      <button onClick={this.handleClick}>
+        {this.state.isToggleOn ? 'ON' : 'OFF'}
+      </button>
+    )
   }
 }
 
@@ -49,11 +76,30 @@ const user = {
   lastName: "Bloggs"
 };
 
+function activateLasers() {
+  console.log("fire those lazors");
+}
+
+function ActionLink() {
+  function handleClick(e) {
+    e.preventDefault();
+    console.log("The link was clicked.");
+  }
+
+  return (
+    <a href="#" onClick={handleClick}>
+      Click me
+    </a>
+  );
+}
+
 const element = (
   <div>
     <Welcome user={user} />
     <Clock />
+    <ActionLink />
+    <button onClick={activateLasers}>Activate Lasers</button>
+    <Toggle />
   </div>
-
 );
 ReactDOM.render(element, document.getElementById("root"));
